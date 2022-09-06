1326b3c56cc94d5ab1cea86ff769c54e93147cad
Angelica Islas <102178971+NewAngie@users.noreply.github.com>
2022-09-05 22:09:06 -0500

    "Prueba"

---
+++ SameAccountGit.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class SameAccountGit {
+    public static void main(String[] args) {
+        System.out.println("Esta es una prueba con la misma cuenta de Git haciendo commit y creando una nueva clase");
+    }
+}
